a4e6812f5f00685df9377c688109b12c1648ce88
Salsabila <bella.irbah@gmail.com>
2021-03-27 18:18:21 +0700

    1a fix

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#
+
 cat syslog.log | cut -f4- -d "[" | cut -f6- -d " "
 
-#jmlError=`grep -E -o 'ERROR.*' /home/salsabila/soal-shift-sisop-modul-1-I09-2021/soal1/syslog.log |
- #cut -f2- -d\ |
- #cut -f1 -d"(" |
- #sort |
- #uniq -c |
- #sort -n -r`
-#echo  "$jmlError"
+jmlError=`grep -E -o 'ERROR.*' /home/salsabila/soal-shift-sisop-modul-1-I09-2021/soal1/syslog.log |
+ cut -f2- -d\ |
+ cut -f1 -d"(" |
+ sort |
+ uniq -c |
+ sort -n -r`
+echo  "$jmlError"
